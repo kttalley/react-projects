fe985389a2ad1b304b3a981d915e42ae096fdf29
kttalley <talleykristian@gmail.com>
2018-05-31 23:24:43 -0700

    created first react component using both react/reactDOM

---
+++ react-visual-sketches/react-doodle1/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import logo from './logo.svg';
+import './App.css';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+class Hello extends React.Component {
+  render() {
+    return <h1>Hello World</h1>;
+  }
+}
+
+// ReactDOM.render(
+//   <Hello/>,
+//   document.getElementById('root')
+// );
+
+
+
+
+export default Hello;
