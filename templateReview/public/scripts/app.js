1d2f521562228626badfee42f6d308571ddb2378
Kristian Talley <talleykristian@gmail.com>
2018-10-29 18:01:52 -0700

    more review

---
+++ templateReview/public/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+console.log('app.js is running ok?');
+
+var template = React.createElement(
+  'h1',
+  null,
+  'ChickenbUTTwwwwww.'
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
---
+++ templateReview/src/app.js
@@ -0,0 +1,6 @@
+console.log('app.js is running ok?');
+
+let template = <h1>ChickenbUTTwwwwww.</h1>;
+var appRoot = document.getElementById('app')
+
+ReactDOM.render(template, appRoot);
